8b80ed56b5662be19ee393e38a8a404c48a08aa1
Dracula <captain@fantasyui.com>
2016-11-04 19:39:35 -0400

    Usage and prep for reporters.

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 process.stdin.setEncoding('utf8');
 const setup = require('commander');
 const program = require('./program.js');
-setup .version('1.0.0');
+setup
+  .version('1.0.0')
+  .usage('[options] <old.css> <new.css>')
+  .option('-r, --reporter <type>', 'reporter name [json|text]', 'text');
+
 setup.parse(process.argv);
 let data = null;
 program(setup, data);
