d6414beeebf526636d10b978f5ae4597443eea39
Dracula <captain@fantasyui.com>
2016-11-04 19:55:40 -0400

    Unit test

---
+++ test.js
@@ -0,0 +1,7 @@
+require(__dirname+'/program.js')({reporter: 'json', args:['test/old.css','test/new.css']}, {}, function(error,data){
+    if(JSON.stringify(data) === `{"meta":{"olderFilename":"test/old.css","newerFilename":"test/new.css","olderSelectorCount":3,"newerSelectorCount":3},"data":{"deleted":[".b"],"created":[".x"]}}`){
+      process.exit();
+    }else{
+      process.exit(1);
+    }
+});
