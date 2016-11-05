58738b67045378a7e7ad734432b4fc939f190733
Dracula <captain@fantasyui.com>
2016-11-04 20:04:33 -0400

    json/text reporter support

--- program.js
+++ program.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const listCssClasses = require('list-css-classes');
 
-module.exports = function(setup, data){
+module.exports = function(setup, data, done){
 
     console.warn("Comparing [%s] against [%s]", setup.args[0], setup.args[1]);
 
@@ -16,22 +16,49 @@ module.exports = function(setup, data){
       let olderList = values[0].selectors;
       let newerList = values[1].selectors;
 
-      console.warn('Older has %s selectors', Object.keys(olderList).length)
-      console.warn('Newer has %s selectors', Object.keys(newerList).length)
+      // Strategy: create an object that can be used as "JSON reporter",
+      // and then just print it out as text if reporter is type text.
 
-      console.warn(`\nList of ${olderFilename} classes removed from ${newerFilename}:\n`)
+      let report = {
+        meta:{
+          olderFilename,
+          newerFilename,
+          olderSelectorCount: Object.keys(olderList).length,
+          newerSelectorCount: Object.keys(newerList).length,
+        },
+        data:{
+          deleted:[],
+          created:[]
+        },
+      };
+
+
       Object.keys(olderList).sort().forEach(name => {
         if(!newerList[name]){
-          console.log(`${name}`);
+          report.data.deleted.push(name);
         }
       });
 
-      console.warn(`\nList of new classes added to ${newerFilename}:\n`)
       Object.keys(newerList).sort().forEach(name => {
         if(!olderList[name]){
-          console.log(`${name}`);
+          report.data.created.push(name);
         }
       });
+
+
+      if(setup.reporter === 'json'){
+        console.log(JSON.stringify(report, null, '  '));
+        if(done) done(null, report);
+        return;
+      }
+
+      console.warn('Older has %s selectors', report.meta.olderSelectorCount);
+      console.warn('Newer has %s selectors', report.meta.newerSelectorCount);
+
+      console.warn(`\nList of ${olderFilename} classes removed from ${newerFilename}:\n`)
+      report.data.deleted.map(name => console.log(name));
+      console.warn(`\nList of new classes added to ${newerFilename}:\n`)
+      report.data.created.map(name => console.log(name));
 
     });
 }
